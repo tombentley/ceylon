775b204f9d916bbec5542ba10e7a0520d70c3c62
Tom Bentley <tom.bentley@cantab.net>
2016-03-15 09:34:45 +0000

    fix npe

--- compiler-java/src/com/redhat/ceylon/compiler/java/launcher/Main.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/launcher/Main.java
@@ -303,7 +303,7 @@ public class Main extends com.redhat.ceylon.langtools.tools.javac.main.Main {
                     // We also assume our own errors are not abnormal
                     && !ex.getClass().getName().startsWith("com.redhat.ceylon.")
                     // And neither are CCEs from the javac code abnormal
-                    && !(ex instanceof ClassCastException && ex.getMessage().contains("com.redhat.ceylon.langtools."))) {
+                    && !(ex instanceof ClassCastException && ex.getMessage() != null && ex.getMessage().contains("com.redhat.ceylon.langtools."))) {
                 return true;
             }
             return false;
