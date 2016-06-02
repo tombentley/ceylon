c590458adf5c4c1e916ded669c65b09f54a4e854
Tom Bentley <tom.bentley@cantab.net>
2016-06-02 15:00:43 +0100

    Parameterize tests for java8
    Part of JDK8 interfaces

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/cmr/CMRHTTPTests.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/cmr/CMRHTTPTests.java
@@ -51,6 +51,10 @@ import com.sun.net.httpserver.HttpServer;
 
 public class CMRHTTPTests extends CompilerTests {
 
+    public CMRHTTPTests(String[] compilerArgs) {
+        super(compilerArgs);
+    }
+
     interface ExpectedError {}
     
     enum TimeoutIn implements ExpectedError {
